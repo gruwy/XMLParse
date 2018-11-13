a49de877bfec365f0b76913dae0a38657b358a9b
Vladimir Semjonov <38432521+gruwy@users.noreply.github.com>
2018-11-13 12:15:43 +0200

    Add files via upload

--- app/src/main/java/vs/MainActivity.java
+++ app/src/main/java/vs/MainActivity.java
@@ -19,6 +19,8 @@ import static java.lang.System.in;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String URL = "http://127.0.0.1:8080";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,63 +29,11 @@ public class MainActivity extends AppCompatActivity {
         ListView listView;
         listView = findViewById(R.id.listView);
 
-        ArrayList<String> list = new ArrayList<>();
+    }}
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, list);
-        listView.setAdapter(adapter);
 
-    }
 
-    private static final String ns = null;
 
-    public List parse(InputStream in) throws XmlPullParserException, IOException {
-        try {
-            XmlPullParser parser = getResources().getXml(R.xml.app);
-            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-            parser.setInput(in, null);
-            parser.nextTag();
-            return readBuilding(parser);
-        } finally {
-            in.close();
-        }
-    }
 
-    private List readBuilding(XmlPullParser parser) throws XmlPullParserException, IOException {
-        List entries = new ArrayList();
 
-        parser.require(XmlPullParser.START_TAG, ns, "building");
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            String name = parser.getName();
 
-            if (name.equals("object")) {
-                entries.add(readBuilding(parser));
-            } else {
-                skip(parser);
-            }
-        }
-        Log.d("entries", String.valueOf(entries));
-        return entries;
-
-    }
-
-    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
-        if (parser.getEventType() != XmlPullParser.START_TAG) {
-            throw new IllegalStateException();
-        }
-        int depth = 1;
-        while (depth != 0) {
-            switch (parser.next()) {
-                case XmlPullParser.END_TAG:
-                    depth--;
-                    break;
-                case XmlPullParser.START_TAG:
-                    depth++;
-                    break;
-            }
-        }
-    }
-}
---
+++ app/src/main/java/vs/XMLParser.java
@@ -0,0 +1,121 @@
+package vs.xmlparse;
+
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class XMLParser {
+
+    private static final String ns = null;
+
+    private List<InsideXML> parse() throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.nextTag();
+            return readBuilding(parser);
+        } finally {
+        }
+    }
+
+    private List<InsideXML> readBuilding(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List entries = new ArrayList<InsideXML>();
+
+        parser.require(XmlPullParser.START_TAG, ns, "building");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+
+            if (name.equals("object")) {
+                entries.add(readBuilding(parser));
+            } else {
+                skip(parser);
+            }
+        }
+        return entries;
+    }
+
+    public static class InsideXML {
+
+        public final String title;
+        public final String description;
+
+        public InsideXML(String title, String description) {
+            this.title = title;
+            this.description = description;
+        }
+    }
+
+    public InsideXML readObject(XmlPullParser parser, String ns) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "object");
+        String title = null;
+        String description = null;
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("title")) {
+                title = readTitle(parser);
+            } else if (name.equals("description")) {
+                description = readDescription(parser);
+            } else {
+                skip(parser);
+            }
+        }
+        return new InsideXML(title, description);
+    }
+
+
+    // Processes title tags in the feed.
+    public String readTitle(XmlPullParser parser) throws IOException, XmlPullParserException {
+
+        parser.require(XmlPullParser.START_TAG, ns, "title");
+        String title = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "title");
+        return title;
+    }
+
+    // Processes summary tags in the feed.
+    public String readDescription(XmlPullParser parser) throws IOException, XmlPullParserException {
+
+        parser.require(XmlPullParser.START_TAG, ns, "description");
+        String summary = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "description");
+        return summary;
+    }
+
+    // For the tags title and summary, extracts their text values.
+    public String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
+}
